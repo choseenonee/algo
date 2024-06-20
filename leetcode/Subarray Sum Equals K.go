b6b96f67ab4a05b51763642c9c9f681258f37b93
choseenonee <okaymans44@gmail.com>
2024-06-20 17:14:15 +0300

    subarray

--- leetcode/Subarray Sum Equals K.go
+++ leetcode/Subarray Sum Equals K.go
@@ -3,33 +3,47 @@ package leetcode
 func SubarraySum(nums []int, k int) int {
 	var curSum int
 	var ans int
+
 	for left := 0; left < len(nums); {
 		curSum += nums[left]
-		if curSum >= k {
+		if curSum == k {
+			ans += 1
+		}
+
+		var wentRight = false
+		for right := left + 1; right < len(nums); right += 1 {
+			wentRight = true
+			curSum += nums[right]
 			if curSum == k {
 				ans += 1
 			}
-			curSum = 0
-			left += 1
-		} else {
-			var wentRight = false
-			for right := left + 1; right < len(nums); right += 1 {
-				wentRight = true
-				curSum += nums[right]
-				if curSum >= k {
-					if curSum == k {
-						ans += 1
-					}
-					curSum = 0
-					left += 1
-					break
-				}
-			}
-			if !wentRight {
-				break
-			}
 		}
+
+		curSum = 0
+		left += 1
+
+		if !wentRight {
+			break
+		}
+
 	}
 
 	return ans
+}
+
+func SubarraySumSmart(nums []int, k int) int {
+	sum, counter := 0, 0
+	ps := map[int]int{}
+	ps[0] = 1
+
+	for _, n := range nums {
+		sum += n
+
+		rem := sum - k
+		if _, ok := ps[rem]; ok {
+			counter += ps[rem]
+		}
+		ps[sum]++
+	}
+	return counter
 }
